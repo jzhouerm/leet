275c184451dcb53ba8fac5b14542fe9b5a62a797
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2020-12-07 21:13:46 -0500

    12-7-20 brute force method

--- array/two_sum.js
+++ array/two_sum.js
@@ -5,7 +5,19 @@
 //  @param {number} target
 //  @return {number[]}
  
-var twoSum = function(nums, target) {
+// Test cases
+nums = [2,7,11,15], target = 9 //[0,1]
+// nums = [3,2,4], target = 6 //[1,2]
+// nums = [3,3], target = 6 //[0,1]
+
+// cc: [], []
+// cc: [1,1] => []
+// cc: [2,7,2,7] => [0,1] multiple solutions
+
+// Approach #1 (Brute force)
+// Note: The complexity of the brute force algorithm is n^2 which is not acceptable. 
+
+let twoSum = function(nums, target) {
     //for each element in the array in nums array 
     for(let i=0; i<nums.length; i++){
         //iterate over again for each element
@@ -17,3 +29,6 @@ var twoSum = function(nums, target) {
         }
     }
 }
+
+
+twoSum(nums, target) 
