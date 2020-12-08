bad9cd40b836b2210fc487b9759a46cce69b038d
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2020-12-08 00:23:12 -0500

    12-8-20 #136 Single Number

---
+++ array/136_single_number.js
@@ -0,0 +1,28 @@
+
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
+
+// Test cases 
+nums = [2,2,1]          //Output: 1
+// nums = [4,1,2,1,2]      //Output: 4
+// nums = [1]              //Output: 1
+
+
+let singleNumber = function(nums) {
+  let set = new Set(); // use a set for O(1) lookups
+
+  for (num of nums) {
+    /* add the number if it's not in the set, but remove it if it is. this way, after every number has been iterated over, the only number remaining is the number that only shows up once */
+    if (set.has(num)) {
+      set.delete(num);
+    } else {
+      set.add(num);
+    }
+  }
+
+  // there will only be one number remaining in the set. return it
+  return Array.from(set)[0];   
+};
+
+// singleNumber(nums)
+
+console.log(singleNumber(nums))
