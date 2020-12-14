473cc4f846a8a8e5236f4302773b4103e85c1f5a
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2020-12-13 19:08:36 -0500

    12-13-20

--- array/scrap.js
+++ array/scrap.js
@@ -21,3 +21,36 @@ const obj ={
   
   console.log(obj)
 //   { x: 1 } //cannot add values or edit values to original object
+
+
+
+
+
+
+
+
+
+const obj ={
+  x:1
+}
+
+obj['y'] = 2
+obj['a'] = 2
+
+console.log(obj) 
+//   { x: 1, y: 2, a: 2 }
+
+//Write code so key/value pairs cannot be added
+const obj ={
+  x:1
+}
+
+Object.defineProperty(obj, 'y', {
+  writable: false
+})
+
+obj['y'] = 2
+
+console.log(obj)
+//   { x: 1 } //cannot add values or edit values to original object
+
