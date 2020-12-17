a8aefd9a13ef301390000b5ed33da69f4bdd7411
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2020-12-17 18:47:13 -0500

    12-17-20

--- array/scrap.js
+++ array/scrap.js
@@ -24,4 +24,3 @@ const obj ={
 //   { x: 1 } //cannot add values or edit values to original object
 //   { x: 1 } //cannot add values or edit values to original object
 //   { x: 1 } //cannot add values or edit values to original object
-//   { x: 1 } //cannot add values or edit values to original object
