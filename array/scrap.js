778491bdc3282c172fd1f5ca4a1136abb2326dcd
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2020-12-26 20:47:26 -0500

    after stashing

--- array/scrap.js
+++ array/scrap.js
@@ -22,4 +22,4 @@ const obj ={
   console.log(obj)
 //   { x: 1 } //cannot add values or edit values to original object
 //   { x: 1 } //cannot add values or edit values to original object
-
+//after stashing
