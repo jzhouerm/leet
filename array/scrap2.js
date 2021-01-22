2d97d6ebb198dccdd1dce1fd34c7b57dcac1078c
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-21 22:05:17 -0500

    BallotReady Code

--- array/scrap2.js
+++ array/scrap2.js
@@ -1,17 +1,35 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('works for me')
+// })
+
+// function mystery(input) {
+//     // The input will be an array of strings
+//     let index = 0
+//     let flag = false
+//     while ( index < input.length ) {
+//         var word = input[index]
+//         input.forEach(thisWord => {
+//             flag = flag || thisWord === word
+//             console.log("inside the loop", flag, thisWord, word)
+//         })
+//         index++
+//     }
+//     return flag
+
+// }
+
 function mystery(input) {
     // The input will be an array of strings
-    let index = 0
     let flag = false
-    while ( index < input.length ) {
-        var word = input[index]
-        input.forEach(thisWord => {
-            flag = flag || thisWord === word
-        })
-        index++
-        // console.log("flag", flag)
+    for( let i=0; i<input.length; i++){
+        if(input[i] === "u"){
+            return "works"
+        }
     }
     return flag
 
 }
 
-mystery(["string1", "string2", "word"])
+console.log(
+mystery("stuff")
+)
