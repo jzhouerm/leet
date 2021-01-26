7d4555cf2b73aa0c83c5053722c63045a73ba0bb
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-25 22:55:51 -0500

    Completed #412-FizzBuzz

--- top_interview_q/412_fizz_buzz.js
+++ top_interview_q/412_fizz_buzz.js
@@ -1,6 +1,20 @@
 var fizzBuzz = function(n) {
     let arr = []
-    
+    for(let i=1; i<=n; i++){
+        if(i%3 === 0 && i%5 !==0){
+            arr.push("Fizz")
+        }
+        if(i%5 === 0 && i%3 !==0){
+            arr.push("Buzz")
+        }
+        if(i%3 === 0 && i%5 ===0){
+            arr.push("FizzBuzz")
+        }
+        if(i%3 !== 0 && i%5 !==0){
+            arr.push(i.toString(10))
+        }
+        // console.log("n=", n, "i=",i)
+    }
     return arr
 }
 
