bca26850fcce09d11a5633c0f0255cc32405dda4
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-26 21:29:36 -0500

    Completed #412-FizzBuzz

--- array/scrap2.js
+++ array/scrap2.js
@@ -1,22 +1,22 @@
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     console.log('works for me')
-// })
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('works for me')
+})
 
-// function mystery(input) {
-//     // The input will be an array of strings
-//     let index = 0
-//     let flag = false
-//     while ( index < input.length ) {
-//         var word = input[index]
-//         input.forEach(thisWord => {
-//             flag = flag || thisWord === word
-//             console.log("inside the loop", flag, thisWord, word)
-//         })
-//         index++
-//     }
-//     return flag
+function mystery(input) {
+    // The input will be an array of strings
+    let index = 0
+    let flag = false
+    while ( index < input.length ) {
+        var word = input[index]
+        input.forEach(thisWord => {
+            flag = flag || thisWord === word
+            console.log("inside the loop", flag, thisWord, word)
+        })
+        index++
+    }
+    return flag
 
-// }
+}
 
 function mystery(input) {
     // The input will be an array of strings
