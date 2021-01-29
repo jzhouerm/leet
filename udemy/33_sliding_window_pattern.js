d14693c1ea4569645824b28b8ed313a4667cda91
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-29 00:49:51 -0500

    Udemy #33 - Sliding Window Pattern

--- udemy/33_sliding_window_pattern.js
+++ udemy/33_sliding_window_pattern.js
@@ -2,29 +2,66 @@
 // and a number called nums. The function should calculate the maximum sum of n 
 // consecutive elements in the array
 
+// Solution #1 O^2 (Inefficient for long arrays)
+
+// function maxSubarraySum(arr, num) {
+
+//     if( num > arr.length){
+//         return null
+//     }
+//     let max = -Infinity;
+//     //set to negative infinity to cover all integers
+
+//     for(let i = 0; i < arr.length - num + 1; i++){
+//         //subtract num from length b/c you can't start adding if there's less than 'num'
+//         //integers remaining in the array
+//         //temp is storing the current value to compare to the max value 
+
+//         temp = 0
+//         for(let j=0; j<num; j++){
+//             temp += arr[i+j]
+//         }
+//         if (temp > max){
+//             max = temp;
+//         }
+//     }
+
+//     return max
+// }
+
+
+
+// console.log(
+//     maxSubarraySum([1,2,5,2,8,1,5], 2)
+// )
+// // => 10 (2+8)
+
+
+
+
+// ************************************************************************************************************************************
+
+// Solution #2 O(n) (Sliding Window Pattern)
+// Start from the beginning of the Array, remove first index and add last index, add and keep sliding until the end of the array
+
 function maxSubarraySum(arr, num) {
 
-    if( num > arr.length){
-        return null
-    }
-    let max = -Infinity;
-    //set to negative infinity to cover all integers
+    let maxSum = 0
+    let tempSum = 0
 
-    for(let i = 0; i < arr.length - num + 1; i++){
-        //subtract num from length b/c you can't start adding if there's less than 'num'
-        //integers remaining in the array
-        //temp is storing the current value to compare to the max value 
+    if(arr.length < num) return null
 
-        temp = 0
-        for(let j=0; j<num; j++){
-            temp += arr[i+j]
-        }
-        if (temp > max){
-            max = temp;
-        }
+    for(let i = 0; i < num; i++){
+        maxSum += arr[i]
     }
 
-    return max
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++){
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum)
+    }
+
+    return maxSum
 }
 
 
