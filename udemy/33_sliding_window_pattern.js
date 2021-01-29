aa8f45e65a2729c9c0bc61dde2c379d6d6fc8c1c
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-29 00:37:08 -0500

    Udemy #33 - Sliding Window Pattern

--- udemy/33_sliding_window_pattern.js
+++ udemy/33_sliding_window_pattern.js
@@ -8,7 +8,11 @@ function maxSubarraySum(arr, num) {
         return null
     }
     let max = -Infinity;
-    for(let i=0; i<arr.length - num + 1; i++){
+    //set to negative infinity to cover all integers
+
+    for(let i = 0; i < arr.length - num + 1; i++){
+        //subtract num from length b/c you can't start adding if there's less than 'num'
+        //integers remaining in the array
         temp = 0
         for(let j=0; j<num; j++){
             temp += arr[i+j]
