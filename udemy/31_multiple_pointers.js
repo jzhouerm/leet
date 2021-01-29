9c6cc385eb4b21dd7369b8a9c8d9d50d75d6151f
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-29 00:16:47 -0500

    Udemy #31 - Multiple Pointers

--- udemy/31_multiple_pointers.js
+++ udemy/31_multiple_pointers.js
@@ -1,7 +1,8 @@
 //Udemy Solution
 function countUniqueValues(arr){
 //The the number of unique integers using a counter
-//input must be sorted
+//input must be already sorted
+//compare each number in arr to i
     if (arr.length === 0) return 0
     let i =0
     for(let j=1; j<arr.length; j++){
