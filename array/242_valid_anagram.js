47afbd974241f53c5f6032242ec6bc23ec6212a0
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-01-28 19:16:38 -0500

    .

--- array/242_valid_anagram.js
+++ array/242_valid_anagram.js
@@ -1,1 +1,53 @@
 // Solution 1
+
+// var isAnagram = function(s, t) {
+//     let sHash = {}
+//     let tHash={}
+//     if(s.length === t.length){
+//         for (let i=0; i<s.length; i++){
+//             sHash[s[i]] ? sHash[s[i]]++ : sHash[s[i]] = 1
+//         } 
+
+//         for (let j=0; j<t.length; j++){
+//             tHash[t[j]] ? tHash[t[j]]++ : tHash[t[j]] = 1
+//         }
+    
+//     }
+//     else{
+//         return false
+//     }
+//     for (let char in sHash){
+//         if(sHash[char] !== tHash[char]){
+//             return false
+//         }
+        
+//     }
+//     return true
+    
+// };
+
+var isAnagram = function(s, t) {
+    let sHash = {}
+    let tHash={}
+    if(s.length === t.length){
+        for (let i=0; i<s.length; i++){
+            sHash[s[i]] ? sHash[s[i]]++ : sHash[s[i]] = 1
+        } 
+
+        for (let j=0; j<t.length; j++){
+            tHash[t[j]] ? tHash[t[j]]++ : tHash[t[j]] = 1
+        }
+    
+    }
+    else{
+        return false
+    }
+    for (let char in sHash){
+        if(sHash[char] !== tHash[char]){
+            return false
+        }
+        
+    }
+    return true
+    
+};
