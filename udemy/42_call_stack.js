2136b3241b6d934eba42bae39b8143e6d2df0997
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-02-02 23:33:19 -0500

    Added call stack

--- udemy/42_call_stack.js
+++ udemy/42_call_stack.js
@@ -1,3 +1,14 @@
 funtion takeShower(){
     return "Showering!"
 }
+
+function eatBreakfast(){
+    let meal = cookFood()
+    return `Eating ${meal}`
+}
+
+function cookFood(){
+    let items = ["Oatmeal", "Eggs", "Protein Shake"]
+    return items[Math.floor(Math.random()*items.length)]
+}
+
