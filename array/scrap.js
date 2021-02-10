db2459f936f40784eec5c3ed3698a9db5641e38c
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-02-10 00:00:03 -0500

    .

--- array/scrap.js
+++ array/scrap.js
@@ -29,5 +29,3 @@ Review for interview
 https://leetcode.com/problems/trapping-rain-water/
 Review for interview
 https://leetcode.com/problems/trapping-rain-water/
-Review for interview
-https://leetcode.com/problems/trapping-rain-water/
