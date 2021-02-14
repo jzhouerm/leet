1368a912cbd0b81e5794b972bee1db3f42aa6e3c
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-02-13 23:47:00 -0500

    .

--- udemy/50_10_power.js
+++ udemy/50_10_power.js
@@ -1,3 +1,5 @@
 /*Coding Exercise #10 - power
-
+Write a function called power which accepts a base and an exponent. The function should
+return the power of the base to the exponent. This function should mimic the functionality of
+Math.pow() - do not worry about negative bases and exponents.
 */
