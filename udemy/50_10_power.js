a26be00a40fc1ece2d570746f6335059adde4c23
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-02-15 23:59:23 -0500

    .

--- udemy/50_10_power.js
+++ udemy/50_10_power.js
@@ -10,7 +10,7 @@ function power(base, exponent){
 
 console.log(
 
-    // power(2,0)  //1
-    // power(2,2)  //4
-    // power(2,4)  //16
+    power(2,0)  //1
+    power(2,2)  //4
+    power(2,4)  //16
 )
