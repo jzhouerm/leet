a54c5b2a820344d394f8191f9a4faedabaf335bc
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-02-16 23:22:02 -0500

    .

--- udemy/33_sliding_window_pattern.js
+++ udemy/33_sliding_window_pattern.js
@@ -1,33 +1,33 @@
-// Write a function called maxSubarraySum which accepts an array of integers 
-// and a number called nums. The function should calculate the maximum sum of n 
-// consecutive elements in the array
+Write a function called maxSubarraySum which accepts an array of integers 
+and a number called nums. The function should calculate the maximum sum of n 
+consecutive elements in the array
 
-// Solution #1 O^2 (Inefficient for long arrays)
+Solution #1 O^2 (Inefficient for long arrays)
 
-// function maxSubarraySum(arr, num) {
+function maxSubarraySum(arr, num) {
 
-//     if( num > arr.length){
-//         return null
-//     }
-//     let max = -Infinity;
-//     //set to negative infinity to cover all integers
+    if( num > arr.length){
+        return null
+    }
+    let max = -Infinity;
+    //set to negative infinity to cover all integers
 
-//     for(let i = 0; i < arr.length - num + 1; i++){
-//         //subtract num from length b/c you can't start adding if there's less than 'num'
-//         //integers remaining in the array
-//         //temp is storing the current value to compare to the max value 
+    for(let i = 0; i < arr.length - num + 1; i++){
+        //subtract num from length b/c you can't start adding if there's less than 'num'
+        //integers remaining in the array
+        //temp is storing the current value to compare to the max value 
 
-//         temp = 0
-//         for(let j=0; j<num; j++){
-//             temp += arr[i+j]
-//         }
-//         if (temp > max){
-//             max = temp;
-//         }
-//     }
+        temp = 0
+        for(let j=0; j<num; j++){
+            temp += arr[i+j]
+        }
+        if (temp > max){
+            max = temp;
+        }
+    }
 
-//     return max
-// }
+    return max
+}
 
 
 
