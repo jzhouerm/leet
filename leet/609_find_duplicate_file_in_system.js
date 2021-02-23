91d88bff89512dd9b7005f9a892ce0ba8b51a598
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-02-23 00:22:38 -0500

    #609

---
+++ leet/609_find_duplicate_file_in_system.js
@@ -0,0 +1,48 @@
+
+// https://leetcode.com/problems/find-duplicate-file-in-system/
+let paths = ["root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"]
+
+// Solution 1 - 196 ms
+var findDuplicate = function(paths) {
+    let fileDic = {};
+    for(let i of paths){
+        var cur = i.split(/\s+/g);
+        var pathS = cur[0];
+        for(let j of cur){
+            var content = j.match(/\([a-z]+\)/g);
+            var file = j.match(/[0-9a-z]+\.txt/g);
+            if(content!=null){
+                if(!fileDic[content]){
+                    fileDic[content]=[];
+                }
+                fileDic[content].push(pathS+"/"+file);
+            }
+        }
+        
+        
+    }
+    let test = Object.values(fileDic).filter(item=>item.length>1);
+    return test;
+};
+
+
+// Solution 2 - 108 ms
+var findDuplicate = function(paths) {
+    let dict = {};
+    let output = [];
+    
+    for (let i = 0; i < paths.length; i++) {
+        let curr = paths[i].split(' ');
+        let root = curr[0] + '/';
+        for (let j = 1; j < curr.length; j++) {
+            let start = curr[j].indexOf('(');
+            let end = curr[j].indexOf(')');
+            let key = curr[j].slice(start + 1, end);
+            if (!dict.hasOwnProperty(key)) {
+                dict[key] = [];
+            }
+            dict[key].push(root + curr[j].slice(0, start));
+        }
+    }
+    return Object.values(dict).filter(arr => arr.length > 1);
+};
