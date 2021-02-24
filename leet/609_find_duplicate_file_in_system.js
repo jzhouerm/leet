f293b3f337bb82b535d900bed2d0ae60e3529b3e
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-02-23 23:19:06 -0500

    #609

--- leet/609_find_duplicate_file_in_system.js
+++ leet/609_find_duplicate_file_in_system.js
@@ -2,53 +2,34 @@
 // https://leetcode.com/problems/find-duplicate-file-in-system/
 let paths = ["root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"]
 
-// Solution 1 - 196 ms
-// var findDuplicate = function(paths) {
-//     let fileDic = {};
-//     for(let i of paths){
-//         var cur = i.split(/\s+/g);
-//         var pathS = cur[0];
-//         for(let j of cur){
-//             var content = j.match(/\([a-z]+\)/g);
-//             var file = j.match(/[0-9a-z]+\.txt/g);
-//             if(content!=null){
-//                 if(!fileDic[content]){
-//                     fileDic[content]=[];
-//                 }
-//                 fileDic[content].push(pathS+"/"+file);
-//             }
-//         }
-        
-        
-//     }
-//     let test = Object.values(fileDic).filter(item=>item.length>1);
-//     return test;
-// };
 
-
-// Solution 2 - 108 ms
+// Solution 1 - 108-164ms
 var findDuplicate = function(paths) {
-    let dict = {};
-    let output = [];
+    let dict = {}
+    let output = []
     
-    for (let i = 0; i < paths.length; i++) {
-        let curr = paths[i].split(' ');             //['r', 'o', 'o', 't', '/', 'a',' ', '1', '.', 't', 'x', 't','(', 'a', 'b', 'c', 'd', ')',' ', '2', '.', 't', 'x', 't','(', 'e', 'f', 'g', 'h', ')']
-        let root = curr[0] + '/';                   //'r/'
-
-        for (let j = 1; j < curr.length; j++) {
-            let start = curr[j].indexOf('(');   
-            let end = curr[j].indexOf(')');     
-            let key = curr[j].slice(start + 1, end);    //content
-            // console.log("curr[j]: ", curr[j], "start: ", start, "end: ", end,  "key: ", key)
-
+    for (let i = 0; i< paths.length; i++){
+        let curr = paths[i].split(' ')
+        let root = curr[0] + '/'
+        
+        for (let j = 1; j < curr.length; j++){
+            let start = curr[j].indexOf('(')
+            let end = curr[j].indexOf(')')
+            let key = curr[j].slice(start + 1, end)
+            // console.log("curr[j]: ", curr[j], "start:", start, "end:", end,  "key:", key, "dict:", dict)
             if (!dict.hasOwnProperty(key)) {
-                dict[key] = [];
+                dict[key] = []
             }
+            // console.log("curr[j]:", curr[j], "dict[key]:", dict[key] )
             dict[key].push(root + curr[j].slice(0, start));
+            // console.log("key: ", key, "exists?", dict.hasOwnProperty(key), dict)
+            
         }
+        
     }
-    // return Object.values(dict).filter(arr => arr.length > 1);
-};
+     return Object.values(dict).filter(arr => arr.length > 1);
+    //Object.values(dict) works but there's spaces b/w arrays, need to filter
+}; //108-164ms
 
 
 
@@ -58,6 +39,6 @@ console.log(
 
 /*
 
-Solution: https://leetcode.com/problems/find-duplicate-file-in-system/discuss/104146/Simple-JavaScript-solution-using-map
+Solution#2: https://leetcode.com/problems/find-duplicate-file-in-system/discuss/104146/Simple-JavaScript-solution-using-map
 */
 
