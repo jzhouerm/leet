7d67006696d44048e7d73d54f13b8ca3f6faa9d4
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-02-25 02:39:52 -0500

    #609

--- leet/609_find_duplicate_file_in_system.js
+++ leet/609_find_duplicate_file_in_system.js
@@ -31,6 +31,54 @@ var findDuplicate = function(paths) {
     //Object.values(dict) works but there's spaces b/w arrays, need to filter
 }; //108-164ms
 
+// Solution 2 - 120-164ms
+var findDuplicate = function(paths) {
+    const map = {};
+    for (let text of paths) {
+        for (let i = 1, files = text.split(' '); i < files.length; i++) {
+            let paren = files[i].indexOf('(');
+            let content = files[i].substring(paren + 1, files[i].length - 1);
+            map[content] = map[content] || [];
+            map[content].push(files[0] + '/' + files[i].substr(0, paren));
+        }
+    }
+    return Object.values(map).filter(dups => dups.length > 1);
+};
+
+
+
+//Solution 152-168ms
+var findDuplicate = function(paths) {
+    //create an object to hold arrays for each set of file contents
+        //i.e.{abcd: [root/a1.txt, root/c/3.txt], efgh: [root/a/2.txt, ...]}
+    let fileContents = {}
+    let result = []
+    //loop over paths array and split(" ")
+    for ( let i = 0; i < paths.length; i++){
+        
+        paths[i] = paths[i].split(" ")
+        //loop each inner path array, starting at j=1
+        //place each file path in the relative fileContents Obj key, joined ith it's directory path
+        for ( let j = 1; j < paths[i].length; j++ ){
+            const [name, contents] = paths[i][j].split('(')
+            if (!fileContents[contents]) {
+                fileContents[contents] = [paths[i][0] + `/${name}`]
+            } else {
+                fileContents[contents].push(paths[i][0] + `/${name}`)
+            }
+        }
+    }
+    
+    // loop through fileContents and push each array into the return array
+    for (const filePath in fileContents) {
+        if (fileContents[filePath]. length > 1) {
+            result.push(fileContents[filePath])
+        }
+    }
+    
+    return result
+}; //152ms-168ms
+
 
 
 console.log(
