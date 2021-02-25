0de7ca9710b03992db12d8390d148c80f0ed6315
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-02-24 23:52:34 -0500

    #322

---
+++ top_interview_q/322_coins.js
@@ -0,0 +1,22 @@
+
+function change(num) {
+    num = Number(num);
+    // Error handles non executable input
+    if (!num || num % 1 !== 0 || num < 0) return null
+  
+    let coins = [25, 10, 5, 1];
+    // Holds coin denominations
+  
+    for (let i = 0; i < coins.length; i++) {
+      let temp = coins[i];
+  
+      coins[i] = Math.floor(num / coins[i]);
+      // Sets array value to lowest value divisible by denomination
+      
+      num %= temp;
+      // Sets num to remainder per previous denomination
+    }
+    return coins.reverse();
+  }
+  
+  console.log(change(33));
