4efb88edb2846e1dd21ddb57e3e1b9219e160a06
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-03-04 23:14:07 -0500

    .

--- top_interview_q/2-11-21/task_2.js
+++ top_interview_q/2-11-21/task_2.js
@@ -1,5 +1,14 @@
 /*
 Return the shortest balanced string, otherwise, return -1
+A string is considered balanced when every letter in the string appears both in uppercase and lowercase
+For example, CATattac is balanced (a, c, t occur in both cases). Madam is not (a, d only appear in lowercase).
+Write a function that given a string returns the shortest balanced substring of that string.
+Can this be solved with a sliding window approach?
+Update:
+More examples
+“azABaabza” returns “ABaab”
+“TacoCat” returns -1 (not balanced)
+“AcZCbaBz” returns the entire string
 */
 
 // let S = "azABaabza" //"ABaab"
