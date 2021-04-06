357c85209182b4ae3855f14885bdf31bfe9e07f5
jzhouerm <60906798+jzhouerm@users.noreply.github.com>
2021-04-06 01:09:11 -0400

    .

---
+++ top_interview_q/other/nisc.js
@@ -0,0 +1,38 @@
+const _ = require('lodash');
+
+const arr = [1,2,3,4,5,6,7]
+const k = 3
+
+
+function sayHello(arr, k) {
+  let newArr = []
+  let currentIndex = 2
+  
+  for (let i=0; i<arr.length; i++) {
+    arr[]
+  }
+  
+  
+}
+
+
+
+
+console.log(
+
+sayHello(arr, k)
+  
+)
+
+// [1,2,3,4,5,6,7]
+// [1,2,4,5,6,7]
+// [1,2,4,5,7]
+// [1,4,5,7]
+// [1,4,5]
+// [1,4]
+// [4]
+// []
+
+// output = [3,6,2,7,5,1,4]
+  
+// NISC interview question
